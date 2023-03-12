3453044d05a4857af2f3f83ea3f1f60ae981d590
Suwanna Truss <suwanna.bgu@gmail.com>
2023-03-12 00:22:43 +0000

    update solution for Codility - FrogRiverOne

--- day7/frog_river_one.py
+++ day7/frog_river_one.py
@@ -5,16 +5,33 @@ https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/
 
 Result:
 https://app.codility.com/demo/results/trainingBUF5C2-Z8Q/
-
+https://app.codility.com/demo/results/trainingM35SQM-88Q/
 '''
 
+def main():
+    test_input = [
+        [5, [1,3,1,4,2,3,5,4]],
+        [6, [1,3,1,4,2,3,5,4]],
+        [6, [3,4,2,3,5,4,6]]  
+    ]
+    for i in range(len(test_input)):
+        print(f'X = {test_input[i][0]}, Array = {test_input[i][1]}')
+        print(f'result: {solution(test_input[i][0], test_input[i][1])}')
 
 def solution(X, A):
-    step = [0] * X
-    for i in range(X):
-        try:
-            step[i] = A.index(i+1)
-        except:
-            return -1
-    return max(step)
+    n = len(A)
+    steps = [0] * (X+1)
+    count = 0
 
+    for i in range(n):
+        if steps[A[i]] == 0:
+            steps[A[i]] += 1
+            count += 1
+        else:
+            continue
+        if count == X:
+            return i
+    return -1
+
+if __name__ == "__main__":
+    main() 
