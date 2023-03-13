1422e5687a8b9333f5bff68d403e320cb9734e5c
Suwanna Truss <suwanna.bgu@gmail.com>
2023-03-13 20:25:23 +0000

    add solution for Codility lesson4 - MissingInteger

--- day9/missing_integer.py
+++ day9/missing_integer.py
@@ -4,8 +4,7 @@ Instruction:
 https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/
 
 result:
-
-
+https://app.codility.com/demo/results/trainingBS3B5J-Y7R/
 '''
 
 def main():
