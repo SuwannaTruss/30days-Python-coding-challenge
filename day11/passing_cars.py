2f658a1b275c6281a632a187c195e7a0c1a03cfa
Suwanna Truss <suwanna.bgu@gmail.com>
2023-03-15 23:14:08 +0000

    update solution for Codility lesson 5 - PassingCars

--- day11/passing_cars.py
+++ day11/passing_cars.py
@@ -4,8 +4,8 @@ https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/
 
 Result:
 https://app.codility.com/demo/results/training4V3VJV-WKQ/
+https://app.codility.com/demo/results/training34JUKW-U3N/
 '''
-
 
 def main():
     test_input = [
@@ -26,9 +26,11 @@ def solution(A):
     count = 0
     suffix_sum = [0] * (len(A)+1)
     for i in reversed(range(len(A))):
-        suffix_sum[i] += A[i]+ suffix_sum[i+1]
+        suffix_sum[i] = A[i]+ suffix_sum[i+1]
         if A[i] == 0:
-            count += suffix_sum[i]       
+            count += suffix_sum[i]  
+        if count > 1000000000:
+            return -1     
     return count
 
 if __name__ == "__main__":
